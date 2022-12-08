4110a3fc6f0a0344b4148f31cc5a69ece3af8f00
Pedro Duarte <pedrohlduarte@gmail.com>
2022-12-08 17:17:35 -0300

    style: remove image nao usado

--- src/components/templates/NextTemplateLandingPage/index.tsx
+++ src/components/templates/NextTemplateLandingPage/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { NextLayout } from '../NextLayout'
 import { NextGridBlurredBackdrop } from '../../organisms/NextGridBlurredBackdrop'
 import { NextCallToAction, NextCallToActionProps } from '../../organisms/NextCallToAction'
